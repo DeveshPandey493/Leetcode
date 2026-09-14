5dfe9481bd458aac03d3440f7bd348fdf833c3ad
DeveshPandey493 <deveshpandey493@gmail.com>
2026-09-14 11:00:24 +0530

    Time: 2 ms (16.33%), Space: 64 MB (99.63%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,45 +1,21 @@
-// class Solution {
-//     public boolean kLengthApart(int[] nums, int k) {
-//         int arr[] = new int[nums.length];
-//         int idk = 0;
-//         for(int i = 0; i < nums.length;i++){
-//             if(nums[i] == 1){
-//                 arr[i] = i;
-//                 idk++;
-//             }
-//         }
-//         for(int i = 0; i < idk ;i++){
-//             if(arr[i+1] - arr[i] >= k){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
-
     public boolean kLengthApart(int[] nums, int k) {
-
         int arr[] = new int[nums.length];
-
         int idk = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-
-            if(nums[i] == 1) {
-                arr[idk] = i;
-                idk++;
+        
+        for(int i = 0; i < nums.length;i++){
+            if(nums[i] == 1){
+                arr[idk++] = i;
+                
             }
         }
 
-        for(int i = 0; i < idk - 1; i++) {
-
-            if(arr[i + 1] - arr[i] - 1 < k) {
+        for(int i = 0; i < idk - 1;i++){
+            if(arr[i+1] - arr[i] - 1 < k){
                 return false;
             }
         }
-
         return true;
     }
 }
+
